97210ac8ea1fcdeb058ede03382427e5435f82a7
DevSoeren29 <93468567+DevSoeren29@users.noreply.github.com>
2024-08-13 19:08:19 +0200

    I

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Get the phone image element
+const phoneImage = document.getElementById('phone-image');
 
+// Add an event listener to the phone image element
+phoneImage.addEventListener('click', () => {
+	// Open a file input dialog to select a new image
+	const fileInput = document.createElement('input');
+	fileInput.type = 'file';
+	fileInput.accept = 'image/*';
+	fileInput.onchange = (e) => {
+		const file = fileInput.files[0];
+		const reader = new FileReader();
+		reader.onload = (e) => {
+			phoneImage.src = e.target.result;
+		};
+		reader.readAsDataURL(file);
+	};
+	fileInput.click();
+});
